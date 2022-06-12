b25869361de5432730701e1ea15e7d9a012ae6b2
5argon <pyasry@gmail.com>
2022-06-12 16:04:49 +0700

    Add slot requirement player card dividers

--- Unity/AHLCG3mmDivider/Assets/Scripts/IconRef.cs
+++ Unity/AHLCG3mmDivider/Assets/Scripts/IconRef.cs
@@ -10,14 +10,24 @@ namespace E7.AhLcgMiniDivider
         [SerializeField] internal GameObject go;
         [SerializeField] internal Image image;
 
+        static Color blackColor = new Color(0.2078431f, 0.1803922f, 0.1215686f);
+
         internal void ApplySpec(GenSpec genSpec)
         {
             var gs = genSpec;
             go.SetActive(gs.iconDisplay);
-            if (gs.setDisplay && gs.iconDisplayIcon != null)
+            if (gs.iconDisplay && gs.iconDisplayIcon != null)
             {
                 image.gameObject.SetActive(true);
                 image.sprite = gs.iconDisplayIcon;
+                if (gs.iconPreserveColor)
+                {
+                    image.color = Color.white;
+                }
+                else
+                {
+                    image.color = blackColor;
+                }
             }
             else
             {
